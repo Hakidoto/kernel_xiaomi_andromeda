a96c65864a233fafbd477516048f6658970fbd44
Yaroslav Furman <yaro330@gmail.com>
2019-06-01 19:11:36 +0300

    drm/atomic: boost DDR bus when committing a new frame
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    Signed-off-by: Hadid Garcia <hadid.garcia1@gmail.com>

--- drivers/gpu/drm/drm_atomic.c
+++ drivers/gpu/drm/drm_atomic.c
@@ -2248,7 +2248,8 @@ int drm_mode_atomic_ioctl(struct drm_device *dev,
 			(arg->flags & DRM_MODE_PAGE_FLIP_EVENT))
 		return -EINVAL;
 
-	}
+	if (!(arg->flags & DRM_MODE_ATOMIC_TEST_ONLY))
+		devfreq_boost_kick(DEVFREQ_CPU_LLCC_DDR_BW);
 
 	drm_modeset_acquire_init(&ctx, 0);
 

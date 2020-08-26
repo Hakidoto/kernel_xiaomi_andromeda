3072fcbd5f8b2116c838cde6a87e3c6761b38f76
Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>
2020-08-26 15:13:07 -0500

    freq_table.c: fix undesired parameter by nuke down it.
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>
    Signed-off-by: Hadid Garcia <hadid.garcia1@gmail.com>

--- drivers/cpufreq/freq_table.c
+++ drivers/cpufreq/freq_table.c
@@ -61,9 +61,6 @@ int cpufreq_frequency_table_cpuinfo(struct cpufreq_policy *policy,
 	if (max_freq > cpuinfo_max_freq_cached)
 		cpuinfo_max_freq_cached = max_freq;
 
-	if (max_freq > cpuinfo_max_freq_cached)
-		cpuinfo_max_freq_cached = max_freq;
-
 	if (policy->min == ~0)
 		return -EINVAL;
 	else

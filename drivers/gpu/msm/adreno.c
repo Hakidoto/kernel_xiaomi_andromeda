c8e6c2077998ceed2ba6d4962c7673136f9a8d4d
Hadid Garcia <hadid.garcia1@gmail.com>
2021-07-31 13:12:53 -0500

    Revert "msm: adreno: Limit default max freq at initcall"
    This reverts commit b1c94476192a4cb5d064602533cb0737baae913c.
    
    Signed-off-by: Hadid Garcia <hadid.garcia1@gmail.com>

--- drivers/gpu/msm/adreno.c
+++ drivers/gpu/msm/adreno.c
@@ -1612,11 +1612,6 @@ static int adreno_init(struct kgsl_device *device)
 
 	}
 
-
-	device->pwrscale.devfreqptr->max_freq = 675000000;
-
-	place_marker("M - DRIVER ADRENO Ready");
-
 	return 0;
 }
 
